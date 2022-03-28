434b8c369dddc0ed0af6649634aad67154649db2
siavosh <siavoshrahimir@gmail.com>
2022-03-28 19:30:35 +0200

    Create basic of token and test against it

---
+++ config-overrides.js
@@ -0,0 +1,12 @@
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
+module.exports = {
+    // The Webpack config to use when compiling your react app for development or production.
+    webpack: function(config, env) {
+        // ...add your webpack config
+
+        config.plugins.push(new NodePolyfillPlugin());
+
+        return config;
+    },
+
+}
---
+++ src/contracts/Token.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Token{
+    string public name = 'DApp Token';
+    string public symbol = 'DAPP';
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    constructor() public{
+        totalSupply = 1000000 * (10 ** decimals);
+    }
+}
+
