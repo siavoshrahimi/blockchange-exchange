cd67bc1543cf4b91bafb01d73231b175ed63454d
siavosh <siavoshrahimir@gmail.com>
2022-07-21 17:04:58 +0200

    "hi"

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,5 @@
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
+
 module.exports = {
     // The Webpack config to use when compiling your react app for development or production.
     webpack: function(config, env) {
